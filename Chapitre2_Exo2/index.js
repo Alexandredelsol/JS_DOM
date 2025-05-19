7eaabbe98077342cf2fab685c11233dde5d0964e
Alexandre <alexandre.delsol@hotmail.fr>
2025-05-19 22:18:04 +0200

    chap 2 exo 2

---
+++ Chapitre2_Exo2/index.js
@@ -0,0 +1,11 @@
+imgCat1 = document.getElementById("first-cat-img");
+imgCat2 = document.getElementById("second-cat-img");
+
+firstDiv = document.getElementById("first-div");
+secondDiv = document.getElementById("second-div");
+
+secondDiv.appendChild(imgCat1);
+firstDiv.appendChild(imgCat2);
+
+console.log(imgCat1);
+console.log(imgCat2);
