94836090b9c1a500355d1b7a2539c4ed1ee562c3
Alexandre <alexandre.delsol@hotmail.fr>
2025-05-20 09:26:02 +0200

    chap 2 exo 3

---
+++ Chapitre2_Exo3/index.js
@@ -0,0 +1,10 @@
+box = document.querySelector(".box");
+title = document.querySelector(".title");
+console.log(title)
+box.style.height = "300px";
+box.style.width = "400px";
+box.style.backgroundColor = "yellow";
+
+title.style.color = "lightblue";
+title.style.fontSize = "60px";
+title.align = "center";
