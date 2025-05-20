0c60a203ef2f97260a3acbe7a26fcca90a7c955b
Alexandre <alexandre.delsol@hotmail.fr>
2025-05-20 09:13:14 +0200

    chap 2 exo 2

--- Chapitre2_Exo2/index.js
+++ Chapitre2_Exo2/index.js
@@ -1,11 +1,6 @@
-imgCat1 = document.getElementById("first-cat-img");
-imgCat2 = document.getElementById("second-cat-img");
+allDogsImg = document.querySelectorAll('.img-dog');
+console.log(allDogsImg)
 
-firstDiv = document.getElementById("first-div");
-secondDiv = document.getElementById("second-div");
-
-secondDiv.appendChild(imgCat1);
-firstDiv.appendChild(imgCat2);
-
-console.log(imgCat1);
-console.log(imgCat2);
+for (let i = 0; i < allDogsImg.length; i++) {
+    allDogsImg[i].remove();
+}
