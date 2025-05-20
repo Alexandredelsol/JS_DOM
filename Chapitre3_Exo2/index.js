d2f5515bc2b449d59fcff461a38a2d755650ffcf
Alexandre <alexandre.delsol@hotmail.fr>
2025-05-20 14:21:26 +0200

    Todo amélioration

--- Chapitre3_Exo2/index.js
+++ Chapitre3_Exo2/index.js
@@ -7,7 +7,22 @@ const todoList = document.querySelector("#todoList")
 form.addEventListener("submit", event => {
     event.preventDefault();
     let li = document.createElement("li")
-
+    let button = document.createElement("button");
+    button.innerText = "Task Done";
     li.innerText += todoList.value;
     myList.appendChild(li);
+    myList.appendChild(button)
+    button.addEventListener("click", function (event) {
+        event.preventDefault();
+        li.remove();
+        button.remove();
+    })
 })
+
+
+
+/*
+form.addEventListener("click", event => {
+    event.preventDefault();
+    li.remove();
+})*/
