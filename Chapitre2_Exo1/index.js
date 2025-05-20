4de67a88edec9300873bae0981cfc08f81e9f903
Alexandre <alexandre.delsol@hotmail.fr>
2025-05-20 14:01:27 +0200

    Readme change

--- Chapitre2_Exo1/index.js
+++
@@ -1,7 +0,0 @@
-const card2 = document.querySelector(".second-card");
-const imgCat = document.createElement("img");
-imgCat.src = "https://placecats.com/408/287";
-imgCat.alt = "Ceci est un chat";
-console.log(imgCat);
-console.log(card2);
-card2.appendChild(imgCat);
---
+++ README.md
@@ -0,0 +1,3 @@
+# Exercices cours DOM
+
+https://satin-truffle-ac4.notion.site/DOM-1d72ae802e298060acc2f8e22d0915de
