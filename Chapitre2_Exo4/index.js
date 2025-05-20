b18d0a76003ad03796c2c85f297d495200293d3b
Alexandre <alexandre.delsol@hotmail.fr>
2025-05-20 09:37:56 +0200

    chap 2 exo 4

---
+++ Chapitre2_Exo4/index.js
@@ -0,0 +1,7 @@
+div = document.createElement("div");
+containerBoxes = document.querySelector(".container-boxes");
+body = document.querySelector("body");
+console.log(containerBoxes);
+div.classList.add("box");
+
+containerBoxes.appendChild(div);
